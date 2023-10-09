6181ea45d44568c70438b856b16c54731523ebbb
Luigidefilippo <defilippoluigi65@gmail.com>
2023-10-09 14:49:52 +0200

    Richiesta Get tramite postman

---
+++ la-mia-pizzeria-static/Controllers/API/PizzasController.cs
@@ -0,0 +1,26 @@
+﻿using la_mia_pizzeria_static.Database;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace la_mia_pizzeria_static.Controllers.API
+{
+	[Route("api/[controller]/[action]")]
+	[ApiController]
+	public class PizzasController : ControllerBase	
+	{
+		[HttpGet]
+		public ActionResult GetPizze()
+		{
+			using (PizzaContext db = new PizzaContext()) 
+			{
+				List<Pizza> pizze = db.Pizze.Include(pizza => pizza.Gusti).ToList();
+
+				return Ok(pizze);
+			}
+			
+		}
+	}
+}
